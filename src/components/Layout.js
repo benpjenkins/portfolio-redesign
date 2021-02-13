5b0dca3812c2a086523c238fe3dc0dc5d1e0febc
Ben Jenkins <ben.jenkins@icloud.com>
2021-02-13 13:22:14 -0600

    initial commit

---
+++ src/components/Layout.js
@@ -0,0 +1,38 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import { Header } from "./Header"
+import { Footer } from "./Footer"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
